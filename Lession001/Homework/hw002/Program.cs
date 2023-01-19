9afd39859bd27b9af12f4c45bf5b515b4d014020
Дормидонтов Александр Владимирович <a.dormidontov@govirk.ru>
2023-01-19 18:24:23 +0800

    максимальное из трех чисел

---
+++ Lession001/Homework/hw002/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 4: Напишите программу, 
+//которая принимает на вход три числа
+//и выдаёт максимальное из этих чисел.
+//
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.WriteLine($"максимальное число : {a}");
+    }
+    else
+    {
+        Console.WriteLine($"максимальное число : {c}");
+    }
+}
+else if (b > c)
+{
+    Console.WriteLine($"максимальное число : {b}");
+}
+else
+{
+    Console.WriteLine($"максимальное число : {c}");
+}
