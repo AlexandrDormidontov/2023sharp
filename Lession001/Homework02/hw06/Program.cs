b4b86fa02507db76cdd452c68018b0806b1516c0
Дормидонтов Александр Владимирович <a.dormidontov@govirk.ru>
2023-01-19 19:02:08 +0800

    вывести третью цифру в заданном числе

--- Lession001/Homework02/hw06/Program.cs
+++ Lession001/Homework02/hw06/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 13: 
+//Напишите программу, которая выводит третью
+//цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number > 99)
+{
+    int num = (int)Math.Floor(Math.Log10(number))-2;
+    int num2 = (int)Math.Pow(10, num);
+    Console.WriteLine((number / num2 % 10) % 10);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
