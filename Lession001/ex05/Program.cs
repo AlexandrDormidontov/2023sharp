67fd10e54849409f4cd275a27a0d3e1955050dfc
Дормидонтов Александр Владимирович <a.dormidontov@govirk.ru>
2023-01-19 16:38:39 +0800

    целые числа в промежутке

--- Lession001/ex05/Program.cs
+++ Lession001/ex05/Program.cs
@@ -4,16 +4,13 @@
 //4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
 //2 -> " -2, -1, 0, 1, 2"
 
-Console.WriteLine("start");
-Console.WriteLine("Введите число: ");
-int N = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("найти количество четных чисел от 1 до " + N);
 
-for (int i = 1; i <= N; i++)
+Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int i = x *(-1);
+while (i<=x)
 {
-    if (i % 2 == 0)
-    {
-       Console.WriteLine(i++);
-    }
+  Console.WriteLine(i);
+  i++;
 }
-Console.WriteLine("end");
+
