23e77e1b58f102d762bdd50ee5e97689145b3d31
AlexandrDormidontov <adormidon@mail.ru>
2023-01-22 19:00:05 +0800

    изучаем массив строк

--- Lession001/lession003/ex11/Program.cs
+++ Lession001/lession003/ex11/Program.cs
@@ -1,3 +1,19 @@
 ﻿//изучаем массивы
-string[] = {f1, f2, f3, "djon", f5};
+string f1 = "Anna";
+string f2 = "Bob";
+string f3 = "Alex";
+string f4 = "Ira";
+string f5 = "Mila";
+
+string [] f;
+
+string[] = {f1, f2, f3, "John", f5};
+
+int[] = num;
+int num = new int[5];// запись массива из 5 чисел
+int num = {1, 2, 3, 4, 5};// здесь та же самая запись из 5 чисел
+int num = new [] {1, 2, 3, 4, 5}// таже самая запись, ошибкой не будет
+
+
+
 Console.WriteLine(f{1});
