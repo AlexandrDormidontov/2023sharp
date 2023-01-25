3c17988448f38c50bcae2652d3a481569cf348da
Дормидонтов Александр Владимирович <a.dormidontov@govirk.ru>
2023-01-25 19:20:00 +0800

    перерыв

--- Lession001/lession003/ex14/Program.cs
+++ Lession001/lession003/ex14/Program.cs
@@ -7,10 +7,13 @@
 //A (7,-5); B (1,-1) -> 7,21
 
 Console.WriteLine("Введите координату х1: ");
-int x1 = Convert.ToInt32(Console.ReadLine());
+double x1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите координату х2: ");
-int x2 = Convert.ToInt32(Console.ReadLine());
+double x2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите координату y1: ");
-int y1 = Convert.ToInt32(Console.ReadLine());
+double y1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите координату y2: ");
-int y2 = Convert.ToInt32(Console.ReadLine());
+double y2 = Convert.ToInt32(Console.ReadLine());
+
+double s = Math.Sqrt((Math.Pow(x1 - y1),2) +(Math.Pow(x2 - y2),2));
+Console.WriteLine($"расстояние между точками в 2D плоскости = {s}");
