38b0db5839e598ad576a805614734a13b4d0c94d
Дормидонтов Александр Владимирович <a.dormidontov@govirk.ru>
2023-01-27 18:59:16 +0800

    задача про произведение чисел от 1 до N

--- Lession001/lession004/ex19/Program.cs
+++ Lession001/lession004/ex19/Program.cs
@@ -3,3 +3,17 @@
 //выдаёт произведение чисел от 1 до N.
 //4 -> 24 
 //5 -> 120
+
+
+int Proiz(int A)
+{
+    int pr = 1;
+    for (int i = 1; i <= A; i++)
+    {
+      pr = pr * i;  
+    }
+    return pr;
+}
+Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($" {A} -> {Proiz(A)}");
