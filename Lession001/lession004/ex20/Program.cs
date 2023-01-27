4712ea64a8a7e116e10289a229ef9042eed6d490
AlexandrDormidontov <adormidon@mail.ru>
2023-01-27 22:27:33 +0800

    задача про массиы с 1 и 0

--- Lession001/lession004/ex20/Program.cs
+++ Lession001/lession004/ex20/Program.cs
@@ -2,3 +2,11 @@
 //которая выводит массив из 8 элементов, 
 //заполненный нулями и единицами в случайном порядке.
 //[1,0,1,1,0,1,0,0]
+
+int[] array = new int[8];
+Console.WriteLine($"Длина массива = {array.Length}");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 2);
+    Console.Write($"{array[i]}");
+}
