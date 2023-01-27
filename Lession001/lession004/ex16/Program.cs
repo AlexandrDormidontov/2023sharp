551ee90f8e1095617319770f0d5e7d25e5e712a2
Дормидонтов Александр Владимирович <a.dormidontov@govirk.ru>
2023-01-27 18:12:44 +0800

    создание метода

--- Lession001/lession004/ex16/Program.cs
+++ Lession001/lession004/ex16/Program.cs
@@ -8,24 +8,45 @@
 
 //SayLang();
 
-//void SayLang(strins lang)
+//void SayLang(string lang)
 //{
 //  Console.WriteLine("привет, {lang}");  
 //}
-//string lang1 = javascript";
+//string lang1 = "javascript";
 //SayLang (string1);
 //
 //SayLang("PHP");
 //SayLang("werty");
 
-int NumSum(int numA, int numB) //формирование метода нахождения суммы 2х чисел
-{
-    int result = numA + numB;
-    return result;
-}
+//int NumSum(int numA, int numB) //формирование метода нахождения суммы 2х чисел
+//{
+//    int result = numA + numB;
+//    return result;
+//}
 
-int x = NumSum(3,7);
+//int x = NumSum(3,7);
+///Console.WriteLine(x);
+
+//int y = NumSum (2,6);
+//Console.WriteLine(y);
+
+int SumNum(int numA, int numB) //описание метода
+{
+    if (numA >numB)
+    {
+        int result = numA - numB;
+        return result;
+    }
+    else
+    {
+        int result = numB - numA;
+        return result;
+    }  
+}
+int x = SumNum(3, 2);
 Console.WriteLine(x);
 
-int y = NumSum (2,6);
+int y = SumNum(4, 7);
 Console.WriteLine(y);
+
+Console.WriteLine(SumNum(19, 24));
