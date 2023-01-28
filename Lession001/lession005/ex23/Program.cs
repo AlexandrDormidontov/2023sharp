df4dd72c0cf0e707daed684cf0c26e0ce4f13096
AlexandrDormidontov <adormidon@mail.ru>
2023-01-28 21:06:39 +0800

    задача на занятии

---
+++ Lession001/lession005/ex23/Program.cs
@@ -0,0 +1,31 @@
+﻿Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+4; массив [6, 7, 19, 345, 3] -> нет
+-3; массив [6, 7, 19, 345, 3] -> да
+
+
+
+int[] array = new int[5];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-101, 101);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j] == num)
+    {
+        Console.WriteLine("Да, такое число есть в массиве.");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+        break;
+    }
+    
+}
