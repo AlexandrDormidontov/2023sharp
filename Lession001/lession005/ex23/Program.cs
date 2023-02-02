c46c16acbf267fb4e22429c072ea87d45e1e80fe
Дормидонтов Александр Владимирович <a.dormidontov@govirk.ru>
2023-02-02 20:05:39 +0800

    задача про создание массива и поиск заданного числа в массиве 2 способ

--- Lession001/lession005/ex23/Program.cs
+++ Lession001/lession005/ex23/Program.cs
@@ -4,6 +4,34 @@
 //4; массив [6, 7, 19, 345, 3] -> нет
 //-3; массив [6, 7, 19, 345, 3] -> да
 
+//int[] array = new int[5];
+//for (int i = 0; i < array.Length; i++)
+//{
+//    array[i] = new Random().Next(-10, 101);
+//    Console.Write($"{array[i]} ");
+//}
+//Console.WriteLine();
+//
+//Console.Write("Введите число: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int count = 0;
+//
+//for (int j = 0; j < array.Length; j++)
+//{
+//    if (array[j] == num)
+//    {
+//        Console.WriteLine("Да, такое число есть в массиве.");
+//        count++;
+//        break;
+//    }
+//}
+//if (count == 0)
+//{
+//   Console.WriteLine("Нет такого числа в массиве"); 
+//}
+
+
+//обходимся без break и решаем через строки
 int[] array = new int[5];
 for (int i = 0; i < array.Length; i++)
 {
@@ -14,19 +42,14 @@ Console.WriteLine();
 
 Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-int count = 0;
 
+string abc = "Нет такого числа в массиве";
 for (int j = 0; j < array.Length; j++)
 {
     if (array[j] == num)
     {
-        Console.WriteLine("Да, такое число есть в массиве.");
-        count++;
-        break;
+       abc = "Да, такое число есть в массиве.";
     }
+  
 }
-if (count == 0)
-{
-   Console.WriteLine("Нет такого числа в массиве"); 
-}
-
+Console.WriteLine(abc);
