b4364b0be27f2e769cc39439672e02f53a593049
Дормидонтов Александр Владимирович <a.dormidontov@govirk.ru>
2023-02-02 19:14:33 +0800

    задача про создание массива и замены элементов на противоположные с выводом на экран

--- Lession001/lession005/ex22/Program.cs
+++ Lession001/lession005/ex22/Program.cs
@@ -1,22 +1,23 @@
-﻿Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
-[-4, -8, 8, 2] -> [4, 8, -8, -2] 
-
+﻿//Задача 32: Напишите программу замены элементов массива:
+//положительные элементы замените на соответствующие отрицательные, 
+//и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2] 
 
 
 Console.Write("Введите длину массива = ");
-int x = Convert.ToInt32(Console.ReadLine());
+int x = Convert.ToInt32(Console.ReadLine());         //вводим длину массива
 int[] num = new int[x];
 for (int i = 0; i < num.Length; i++)
-    num[i] = new Random().Next(-9, 10);
-foreach (int n in num)
+    num[i] = new Random().Next(-9, 10);             //создаем массив
+foreach (int n in num)             // прогоняем массив по индексам
 {
     Console.Write($"{n} ");
 }
-Console.Write("  -> это изначальный массив");
-for (int i = 0; i < num.Length; i++)
+Console.Write("  -> это изначальный массив"); //выводим массив на экран
+for (int i = 0; i < num.Length; i++)     //прогоняем массив через умножение на мину единицу
     num[i] *= (-1);
 Console.WriteLine();
-foreach (int n in num)
+foreach (int n in num)   //снова выводим массив
 {
     Console.Write($"{n} ");
 }
