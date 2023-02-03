f34db435f006c65848b3bc2986cd7c1b9ce1215c
Дормидонтов Александр Владимирович <a.dormidontov@govirk.ru>
2023-02-03 15:43:30 +0800

    задача про создание массива и количество четных чисел в масиве

---
+++ Lession001/Homework05/hw14/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 34: Задайте массив заполненный случайными 
+//положительными трёхзначными числами. 
+//Напишите программу, которая покажет 
+//количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+Console.Write("Введите длину массива = ");
+int x = Convert.ToInt32(Console.ReadLine());
+int[] num = new int[x];
+
+for (int i = 0; i < num.Length; i++)
+    num[i] = new Random().Next(100, 1000);
+
+int result = 0;
+    for (int i = 0; i < num.Length; i++)
+    {
+       if (num[i] % 2 == 0)
+       {
+        result++;
+       } 
+    } 
+   foreach (int n in num)
+{
+Console.Write($"{n} ");
+}
+Console.Write("  -> это массив из положительных трехзначных чисел    ");
+
+Console.Write($"Число четных чисел: {result}");
+
