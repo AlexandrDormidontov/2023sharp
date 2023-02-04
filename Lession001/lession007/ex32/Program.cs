3762e7636022888fdec347a7ea25a664d177fdf7
AlexandrDormidontov <adormidon@mail.ru>
2023-02-04 20:07:14 +0800

    решение задачи создание массива и определение суммы чисел в главной диагонали

--- Lession001/lession007/ex32/Program.cs
+++ Lession001/lession007/ex32/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 51: Задайте двумерный массив. 
+//Найдите сумму элементов, находящихся 
+//на главной диагонали 
+//(с индексами (0,0); (1;1) и т.д.
+
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Сумма элементов главной диагонали: 
+//1+9+2 = 12
+
+Console.Write("Введите длину массива = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите ширину массива = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] num = new int[m, n];
+int temp = 0;
+for (int i = 0; i < num.GetLength(0); i++)
+{
+    for (int j = 0; j < num.GetLength(1); j++)
+    {
+        num[i, j] = new Random().Next(1, 10);
+        Console.Write($"{num[i, j]} ");
+        if ( i == j)
+        {
+            temp = temp + num[i,j];
+        }
+    }
+    Console.WriteLine();
+    
+}
+Console.WriteLine(temp);
