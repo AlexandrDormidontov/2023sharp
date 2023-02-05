4ea99401149a04bbd41ff9af59df10877934f759
AlexandrDormidontov <adormidon@mail.ru>
2023-02-05 22:05:50 +0800

    задача про пересечение прямыхм

--- Lession001/lession008/ex34/Program.cs
+++ Lession001/lession008/ex34/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 53: Задайте двумерный массив. 
+//Напишите программу, которая поменяет 
+//местами первую и последнюю строку массива.
+
+Console.WriteLine("Масив чисел: ");
+int[,] num = new int[3, 4];
+for (int i = 0; i < num.GetLength(0); i++)
+{
+    for (int j = 0; j < num.GetLength(1); j++)
+    {
+        num[i, j] = new Random().Next(0, 10);
+        Console.Write($"{num[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int i = 0; i < 1; i++)
+{
+    for (int j = 0; j < num.GetLength(1); j++)
+    {
+        int string0 = num[i, j];
+        num[i, j] = num[2, j];
+        num[2, j] = string0;
+    }
+}
+
+for (int i = 0; i < num.GetLength(0); i++)
+{
+    for (int j = 0; j < num.GetLength(1); j++)
+    {
+        Console.Write($"{num[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
