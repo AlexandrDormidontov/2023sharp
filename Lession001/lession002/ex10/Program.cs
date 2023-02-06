44e72da258796dc8c6869173fa9676d0f3113436
AlexandrDormidontov <adormidon@mail.ru>
2023-02-06 23:59:52 +0800

    first commit5

--- Lession001/lession002/ex10/Program.cs
+++ Lession001/lession002/ex10/Program.cs
@@ -1,5 +1,5 @@
 ﻿//У нас есть график, где даны отрезок 1 - (х1; x2) и отрезок 2 (х3, x4)
-​//Необходимо составить код, который находит их пересечение.
+//Необходимо составить код, который находит их пересечение.
 //Пересечением двух отрезка 1 и 2 могут быть - отрезок, точка или ничего
 //С клавиатуры вводим х1, x2, х3, x4 и выводим ответ 
 //- границы пересечений отрезков, общую точку или «не пересекаются».
@@ -8,19 +8,19 @@ Console.WriteLine("Введите x1: ");
 int x1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите x2: ");
 int x2 = Convert.ToInt32(Console.ReadLine());
-onsole.WriteLine("Введите x3: ");
+Console.WriteLine("Введите x3: ");
 int x3 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите x4: ");
 int x4 = Convert.ToInt32(Console.ReadLine());
 if (x2 < x3 || x4 <x1)
 {
-   Console.WriteLine (" не пересекаются") 
+   Console.WriteLine (" не пересекаются"); 
 }
-else if ()
+else if()
 {
-    Console.WriteLine (" общая точка") 
+    Console.WriteLine (" общая точка"); 
 }
 else
 {
-  Console.WriteLine (" пересекается")   
+  Console.WriteLine (" пересекается");   
 }
---
+++ Lession001/myLibrary/Program.cs
@@ -0,0 +1,31 @@
+﻿//методы разные 
+/*void FillArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 100);
+        }
+    }
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+*/
